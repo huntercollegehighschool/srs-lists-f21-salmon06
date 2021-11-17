20b44b22eab5a78fc49a5e9cf5cc8b0a20589c8f
salmon06 <65375261+salmon06@users.noreply.github.com>
2021-11-17 03:50:11 +0000

    done

--- Program2.py
+++ Program2.py
@@ -5,4 +5,8 @@ PROGRAM 2
 Define a function odd_reverse that takes a list as an argument. The function creates a new list containing only the elements in the odd indices (indices 1, 3, 5, ...) and then reverses it. The function returns that list.
 '''
 def odd_reverse(lst):
-  pass #delete this line when you start writing your code
+  oddreverse = []
+  for i in range(0, (len(lst))//2):
+    oddreverse.append(lst[2*i+1])
+  oddreverse.reverse()
+  return oddreverse
--- Program3.py
+++ Program3.py
@@ -5,4 +5,16 @@ PROGRAM 3
 Define a function second_smallest that takes a list of integers or floats as an argument. The function returns the 2nd smallest number in the list.
 '''
 def second_smallest(lst):
-  pass #delete this line when you start writing your code
+  smallest = lst[1]
+  if lst[2] < lst[1]:
+    smallest = lst[2]
+    secondsmallest = lst[1]
+  else:
+    secondsmallest = lst[2]
+  for i in range(0, len(lst)):
+    if lst[i] < smallest:
+      secondsmallest = smallest
+      smallest = lst[i]
+    elif lst[i] > smallest and lst[i] < secondsmallest:
+      secondsmallest = lst[i]
+  return secondsmallest
